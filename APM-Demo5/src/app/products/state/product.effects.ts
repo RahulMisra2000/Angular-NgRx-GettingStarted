8fb84e0b635a378fc7434bac9ab55fe204f3830b
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-10-09 17:41:14 -0400

    What does an Effect really do

--- APM-Demo5/src/app/products/state/product.effects.ts
+++ APM-Demo5/src/app/products/state/product.effects.ts
@@ -14,22 +14,28 @@ import * as productActions from './product.actions';
 @Injectable()
 export class ProductEffects {
 
-  constructor(private productService: ProductService,
-              private actions$: Actions) { }
+  constructor(private productService: ProductService,                     //** Regular Angular Service that makes http calls
+               private actions$: Actions) { }                             // *** This is ALL the actions that are dispatched in the system
 
+  
+  // ***** Each @Effect is basically what Action we want to monitor and then based on some work we do, we dispatch another action
+  // ******************************************************************************************************************************
   @Effect()
   loadProducts$: Observable<Action> = this.actions$.pipe(
-    ofType(productActions.ProductActionTypes.Load),
+    ofType(productActions.ProductActionTypes.Load),                       //*** We are only interested in the Action called Load
     mergeMap(action =>
-      this.productService.getProducts().pipe(
-        map(products => (new productActions.LoadSuccess(products))),
-        catchError(err => of(new productActions.LoadFail(err)))
-      )
+      this.productService.getProducts()
+       .pipe(
+            map(products => (new productActions.LoadSuccess(products))),    //** dispatches an Action called LoadSuccess
+            catchError(err => of(new productActions.LoadFail(err)))         //** dispatches an Action called LoadFail
+       )
     )
   );
+  
+  
 
   @Effect()
-  updateProduct$: Observable<Action> = this.actions$.pipe(
+  updateProduct$: Observable<Action> = this.actions$.pipe(                  //*** We are only interested in the Action called UpdateProduct
     ofType(productActions.ProductActionTypes.UpdateProduct),
     map((action: productActions.UpdateProduct) => action.payload),
     mergeMap((product: Product) =>
@@ -40,6 +46,8 @@ export class ProductEffects {
     )
   );
 
+  
+  
   @Effect()
   createProduct$: Observable<Action> = this.actions$.pipe(
     ofType(productActions.ProductActionTypes.CreateProduct),
